cd263a0878edd67f79ee4905e1892d5888d70555
abhishek <fullstackdeveloper918@gmail.com>
2024-07-25 20:45:39 +0530

    pdf redesign

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Mainpage from "../app/admin/dashboard/page";
 import MemberUpdatePDF from "../components/common/Pdf";
 import SignIn from "../app/auth/signin/page";
+import Pdf from "../components/common/Pdf";
 export default function Home() {
   return (
     <>
       {/* <Mainpage/> */}
-      <SignIn />
+      {/* <SignIn /> */}
+      <Pdf/>
       {/* <MemberUpdatePDF /> */}
     </>
   );
