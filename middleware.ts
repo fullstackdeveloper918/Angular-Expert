92624797db328cd016119ca435d47cc70802d6fc
Abhay <abhay@cybersify.tech>
2024-07-31 20:29:55 +0530

    check

--- middleware.ts
+++ middleware.ts
@@ -4,24 +4,39 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
     const url = request.nextUrl.clone();
 
-    const accessToken = "request.cookies.get('COOKIES_USER_ACCESS_TOKEN')";
-    if (!accessToken) {
-        url.pathname = '/auth/signin';
+    // const cookies:any = parseCookies({ req: request });
+    // let accessToken;
+    // if(!cookies) {
+    //      accessToken = cookies.COOKIES_USER_ACCESS_TOKEN;
+    //     console.log(cookies, "accessToken");
+    // }
+
+    // const url = request.nextUrl.clone();
+
+    // if (accessToken) {
+    //     url.pathname = '/admin/dashboard';
+    //     return NextResponse.redirect(url);
+    // } else {
+    //     url.pathname = '/auth/signin';
+    //     return NextResponse.redirect(url);
+    // }
+    
+    const accessToken = request.cookies.get('COOKIES_USER_ACCESS_TOKEN');
+    console.log(accessToken,"accessToken");
+    
+    if (accessToken) {
+        url.pathname = '/admin/dashboard';
         return NextResponse.redirect(url);
     } else {
-        const staticEmail = 'abhay@gmail.com';
-        const staticPassword = 'Abhay@1';
-
-        if (request.body &&String( request.body) === staticEmail && String(request.body) === staticPassword) {
-            url.pathname = '/dashboard';
-            return NextResponse.redirect(url);
-        }
+        url.pathname = '/auth/signin';
+          return NextResponse.redirect(url);
     }
 
     // Continue to next middleware/route if no specific conditions are met
-    return NextResponse.next();
+    // return NextResponse.next();
 }
 
 export const config = {
-    matcher: ["/",'/admin/dashboard/:path*',  '/admin/member/:path*', '/admin/meeting/:path*','/admin/manage_questions/:path*','/admin/archive_meeting/:path*','/admin/questionnaire/:path*'],
+    matcher: ["/"],
+    // matcher: ["/",'/admin/dashboard/:path*',  '/admin/member/:path*', '/admin/meeting/:path*','/admin/manage_questions/:path*','/admin/archive_meeting/:path*','/admin/questionnaire/:path*'],
 };
