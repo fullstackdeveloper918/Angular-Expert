13f35ab54bc176f8b19d583e27c577af71642316
Abhay <abhay@cybersify.tech>
2024-08-29 19:21:44 +0530

    goals comment

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,11 +1,13 @@
 import React, { Fragment, ReactNode, useEffect } from "react";
 import MainLayout from "../../components/Layout/layout";
 import { parseCookies } from "nookies";
+import NextTopLoader from "nextjs-toploader";
 
 const AdminLayout = ({children}:{children:ReactNode}) => {
   
   return (
     <Fragment>
+     
       <MainLayout>
       {children}
       </MainLayout>
